9809bfccd7b62cd72cda5863d55473e8a3f4a78d
esplett <estherina@gmail.com>
2018-12-05 01:22:27 +0000

    implemented downloadImageByURL to download avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,19 +1,54 @@
 var request = require('request');
+var secrets = require('./secrets.js')
+var fs = require('fs');
+//how do i require the secrets.js file?
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/"
+    + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, JSON.parse(body)); //parsing into an array
+  });
 }
+
+//for loop in here instead of console.logging results
+//loop over array and print each avatar url
+// .avatar_url, array is the result
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+    for (i = 0; i < result.length; i++){
+      console.log(result[i].avatar_url)
+    }
 });
 
-// In the next step you will implement the function to use the request
-// library to programmatically fetch the list of contributors via HTTPS
-// for the given repo.
+
+
+function downloadImageByURL(url, filePath) {
+
+request.get(url)
+  .on('error', function (err) {        // Note 2
+         throw err;
+       })
+       .on('response', function (response, message, headers) {
+         console.log('Downloading image...')    // Note 3
+       })
+
+       .on('end', function() {
+          console.log('Download complete.')
+       })
+
+       .pipe(fs.createWriteStream(filePath))
+}
 
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
 
